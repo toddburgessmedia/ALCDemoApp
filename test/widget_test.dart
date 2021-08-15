7a64974c59fba01fe0efab3c1ce2b01b701002a3
Todd Burgess <tburgess57@gmail.com>
2021-08-15 12:58:09 -0400

    fix comment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -15,7 +9,7 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // Verify that the tabs exist with the proper tab titles
     expect(find.text('Mars'), findsOneWidget);
     expect(find.text('Planets'), findsOneWidget);
     expect(find.text('Web'), findsOneWidget);
