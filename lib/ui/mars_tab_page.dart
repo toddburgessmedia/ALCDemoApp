e375b3eea663d466d0e1bcd0f9d01a1ff0c9e987
Todd Burgess <tburgess57@gmail.com>
2021-08-18 15:46:33 -0400

    clean up the code

--- lib/ui/mars_tab_page.dart
+++ lib/ui/mars_tab_page.dart
@@ -1,4 +1,3 @@
-
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -6,33 +5,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mars_flutter/bloc/mars_cubit.dart';
 import 'package:mars_flutter/bloc/planet_bloc.dart';
 import 'package:mars_flutter/bloc/planet_event.dart';
-import 'package:mars_flutter/model/planet.dart';
 
 import 'mars_tab_page_display.dart';
 
 class MarsTabPage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    // final marsCubit = BlocProvider.of<MarsCubit>(context);
-    // marsCubit.getMars();
     final planetBloc = BlocProvider.of<PlanetBloc>(context);
     planetBloc.add(GetMars());
     return Container(
       padding: EdgeInsets.symmetric(vertical: 16),
-      child: BlocBuilder<PlanetBloc,PlanetState>(
-        builder: (context,state) {
+      child: BlocBuilder<PlanetBloc, PlanetState>(
+        builder: (context, state) {
           if (state is PlanetInitial) {
-            return Text("Initial");
+            return Text("Loading Mars");
           } else if (state is PlanetLoaded) {
-            return MarsTabPageDisplay(state.planet);
+              return MarsTabPageDisplay(state.planet);
+          } else if (state is PlanetError) {
+              return Text("Error loading Mars");
           } else {
-            return Text('unaccounted for state');
+              return Text('unaccounted for state');
           }
         },
       ),
     );
   }
 }
-
